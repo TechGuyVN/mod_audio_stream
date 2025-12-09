a935002c0af2012551ea3df0d48474e16555217e
Hoang Quoc Viet <viethq@vihat.vn>
2025-12-09 11:55:37 +0700

    no message

--- build-mod-audio-stream.sh
+++ build-mod-audio-stream.sh
@@ -8,8 +8,8 @@
 
 apt-get -y install libfreeswitch-dev libssl-dev zlib1g-dev libspeexdsp-dev
 
-# git submodule init
-# git submodule update
+git submodule init
+git submodule update
 
 FS_PKGCONFIG=/usr/local/freeswitch/lib/pkgconfig
 if [ -d "$FS_PKGCONFIG" ]; then
