e34cdbe8b987690b1467e0386f6a57955c06ffc2
Hoang Quoc Viet <viethq@vihat.vn>
2025-12-09 09:59:26 +0700

    no message

--- build-mod-audio-stream.sh
+++ build-mod-audio-stream.sh
@@ -8,14 +8,14 @@
 
 apt-get -y install libfreeswitch-dev libssl-dev zlib1g-dev libspeexdsp-dev
 
-git submodule init
-git submodule update
+# git submodule init
+# git submodule update
 
 FS_PKGCONFIG=/usr/local/freeswitch/lib/pkgconfig
 if [ -d "$FS_PKGCONFIG" ]; then
     export PKG_CONFIG_PATH=$FS_PKGCONFIG
 fi
-
+rm -rf build
 mkdir build && cd build
 cmake -DCMAKE_BUILD_TYPE=Release ..
 make
